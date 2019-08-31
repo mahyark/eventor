c2cbd3056aa43421b4721b358b3f0fdd3e2eac92
mahyark <mahyar.kian@gmail.com>
2019-08-31 12:53:29 +0200

    added sqliteDatabase

---
+++ app/src/main/java/com/eventor/AppDatabase.java
@@ -0,0 +1,37 @@
+package com.eventor;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.eventor.model.Activity;
+import com.eventor.model.ActivityDao;
+
+@Database(entities = {Activity.class}, version = 2, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract ActivityDao activityDao();
+
+    public static AppDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context, AppDatabase.class, "activitydatabase")
+//Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class)
+                            // To simplify the exercise, allow queries on the main thread.
+                            // Don't do this on a real app!
+                            .allowMainThreadQueries()
+                            // recreate the database if necessary
+                            .fallbackToDestructiveMigration()
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
