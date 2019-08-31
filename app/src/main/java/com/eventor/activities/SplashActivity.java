5f72fa00c10592ba65476e14ac57c28b47d34f37
mahyark <mahyar.kian@gmail.com>
2019-08-31 03:33:42 +0200

    added fetch data to splashscreen + progressbar when loading
    data not being saved yet

--- app/src/main/java/com/eventor/activities/SplashActivity.java
+++ app/src/main/java/com/eventor/activities/SplashActivity.java
@@ -1,27 +1,145 @@
 package com.eventor.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.eventor.AppDatabase;
+import com.eventor.HttpHandler;
+import com.eventor.model.Activity;
 import com.eventor.MainActivity;
 import com.eventor.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 public class SplashActivity extends AppCompatActivity {
+    private String TAG = SplashActivity.class.getSimpleName();
+    private Activity activity;
+    private AppDatabase database;
+    private ProgressBar pgsBar;
+    private ViewGroup viewGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        database = AppDatabase.getDatabase(getApplicationContext());
+        viewGroup = (ViewGroup) ((ViewGroup) this
+                .findViewById(android.R.id.content)).getChildAt(0);
+        pgsBar = (ProgressBar)findViewById(R.id.pBar);
 
-        int secondsDelayed = 1;
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+        final List<Activity> activities = database.activityDao().getAllActivity();
+        if (activities.size()==0) {
+            new GetContacts().execute();
+        } else {
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 1000);
+        }
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pgsBar.setVisibility(viewGroup.VISIBLE);
+            Toast.makeText(SplashActivity.this,"Downloading data",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "https://opendata.arcgis.com/datasets/b5f4516df41545299730850103c6443d_643.geojson";
+            String jsonStr = sh.makeServiceCall(url);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray features = jsonObj.getJSONArray("features");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < features.length(); i++) {
+                        JSONObject activity = features.getJSONObject(i);
+                        JSONObject activityProps = activity.getJSONObject("properties");
+                        Log.d("Testing: ", activityProps.toString());
+                        /*
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                        */
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
             }
-        }, secondsDelayed * 1000);
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }
     }
 }
