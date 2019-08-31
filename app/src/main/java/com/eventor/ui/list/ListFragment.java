114f4740325cccba67d6b48539c5e37e92e3ec9c
mahyark <mahyar.kian@gmail.com>
2019-08-31 12:54:09 +0200

    show data from db in list

--- app/src/main/java/com/eventor/ui/list/ListFragment.java
+++ app/src/main/java/com/eventor/ui/list/ListFragment.java
@@ -1,44 +1,57 @@
 package com.eventor.ui.list;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.eventor.AppDatabase;
 import com.eventor.R;
 
 public class ListFragment extends Fragment {
 
-    private String[] items;
+    private List<String> activities;
     private ArrayList<String> listItems;
     private ArrayAdapter<String> adapter;
     private ListView listView;
     private EditText editText;
 
     public void searchItem(String textToSearch){
-        for(String item:items){
-            if(!item.toLowerCase().contains(textToSearch.toLowerCase())){
-                listItems.remove(item);
+        for(String a:activities){
+            if(!a.toLowerCase().contains(textToSearch.toLowerCase())){
+                listItems.remove(a);
             }
         }
         adapter.notifyDataSetChanged();
     }
 
     public void initList(){
-        items = new String[]{"Java","JavaScript","C#","PHP", "С++", "Python", "C", "SQL", "Ruby", "Objective-C"};
-        listItems = new ArrayList<>(Arrays.asList(items));
+        AppDatabase db = AppDatabase.getDatabase(getActivity());
+        List<String> items = new ArrayList<String>(db.activityDao().getActivityNames());
+        activities = items;
+        listItems = new ArrayList<String>(activities);
         adapter = new ArrayAdapter<String>(getContext(), R.layout.list_item, R.id.txtitem, listItems);
         listView.setAdapter(adapter);
+
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -48,6 +61,16 @@ public class ListFragment extends Fragment {
         listView=(ListView)view.findViewById(R.id.listview);
         editText=(EditText)view.findViewById(R.id.txtsearch);
         initList();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getActivity(),
+                        position + " " + listView.getItemAtPosition(position), Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
